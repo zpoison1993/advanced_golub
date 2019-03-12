9bd17a04e95527a98c27bcf3570d1d3d5b91748b
Vladimir <zpoison1993@gmail.com>
2019-03-12 21:33:10 +0300

    work is done

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import sum from './modules/sum';
+import guard from './modules/guard';
+
+console.log(sum(1,2));
+console.log(guard('Dima'));
---
+++ src/modules/guard.js
@@ -0,0 +1,7 @@
+export default function (name) {
+    if(name == "Dima") {
+        return 'hello,Dima! Come in,please..';
+    }else{
+        return 'Get out!';
+    }
+};
